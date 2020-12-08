075fd0b85a65760c184278f531cf6de0be1767ff
joannetuukmangi <s11910041@student.unklab.ac.id>
2020-12-08 15:26:16 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+function helloProfile(){
+    // let value = document.getElementById("message").value;
+    // console.log(value);
+    // alert(value);
+
+    document.querySelector("h1.Personal-profile").style.color = "f1f6f9";
+    document.querySelector("h1.education").style.color = "f1f6f9";
+    document.querySelector("h1.organizational-experience").style.color= "f1f6f9";
+    document.querySelector("h1.seminar-workshop").style.color= "f1f6f9";
+    document.querySelector("h1.contact-me").style.color= "f1f6f9";
+
+    document.querySelector("h1.Personal-profile").style.fontSize= "3.5em";
+    document.querySelector("h1.education").style.fontSize= "3.5em";
+    document.querySelector("h1.organizational-experience").style.fontSize= "3.5em";
+    document.querySelector("h1.seminar-workshop").style.fontSize= "3.5em";
+    document.querySelector("h1.contact-me").style.fontSize= "3.5em";
+
+    document.querySelector("h1.Personal-profile").style.backgroundColor = "ff9b93";
+    document.querySelector("h1.education").style.backgroundColor= "ff9b93";
+    document.querySelector("h1.organizational-experience").style.backgroundColor= "ff9b93";
+    document.querySelector("h1.seminar-workshop").style.backgroundColor= "ff9b93";
+    document.querySelector("h1.contact-me").style.backgroundColor= "ff9b93";
+
+    document.querySelector(".bodies").style.backgroundColor= "ffe0d8";
+    document.getElementById("images").src= "joanne.png";
+}
